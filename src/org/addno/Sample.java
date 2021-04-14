e95d74884516f319c5f89fe4e2c2069fb5c46145
mvinoth15 <indianvkumar@gmail.com>
2021-04-14 21:03:01 +0530

    Sample project

---
+++ src/org/addno/Sample.java
@@ -0,0 +1,15 @@
+package org.addno;
+
+public class Sample {
+	
+	public static  void main(String []args) {
+int a=100;
+for(int i=0;i<=a;i++) {
+	if(i%2!=0)
+	{
+		System.out.println(i);
+	}
+
+}
+	}
+}
